967d95a09be0e522ebc7f9d5ed5d1810a94f20a2
Ròmul <rome.fp@gmail.com>
2024-11-12 20:35:48 +0100

    feat: add vs component

---
+++ the-button/src/app/component/vs/vs.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, input } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList, CdkDragStart, } from '@angular/cdk/drag-drop';
+import { idname } from '../../models/idName';
+
+@Component({
+  selector: 'Btn-vs',
+  standalone: true,
+  imports: [CdkDropList, CdkDrag],
+  templateUrl: './vs.component.html',
+  styleUrl: './vs.component.scss'
+})
+
+export class VsComponent {
+
+  team1 = input.required<idname[]>()
+  team2 = input.required<idname[]>()
+  max4Team = input.required<number>()
+
+  drop(event: CdkDragDrop<idname[]>) {
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      if (event.container.data.length >= this.max4Team()) {
+        transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+        transferArrayItem(event.container.data, event.previousContainer.data, 0, 0);
+
+      } else {
+        transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+      }
+    }
+  }
+
+  drag(started: any) {
+    console.log(started);
+  }
+
+}
