3594901249a2b72a5f7484f184b72bfcb3e578a7
Ròmul <rome.fp@gmail.com>
2024-11-12 20:36:20 +0100

    feat: add vs in modal

--- the-button/src/app/component/modal-booking/modal-booking.component.ts
+++ the-button/src/app/component/modal-booking/modal-booking.component.ts
@@ -4,13 +4,14 @@ import { SearcherComponent } from '../searcher/searcher.component';
 import { MatButtonModule } from '@angular/material/button';
 import { DialogData } from '../../models/dialogData';
 import { idname } from '../../models/idName';
+import { VsComponent } from "../vs/vs.component";
 
 @Component({
     selector: 'app-modal-booking',
     templateUrl: 'modal-booking.component.html',
     styleUrl: 'modal-booking.component.scss',
     standalone: true,
-    imports: [MatDialogModule, SearcherComponent, MatButtonModule],
+    imports: [MatDialogModule, SearcherComponent, MatButtonModule, VsComponent],
 })
 
 export class BookingModal {
@@ -21,16 +22,25 @@ export class BookingModal {
     numberParticipants = new Array(this.data.numberParticipants)
     participants: Array<idname> = []
 
+    team1: Array<idname> = []
+    team2: Array<idname> = []
+
     constructor() {
     }
 
     onOpponentSelected(opponent: idname) {
-        this.participants.push(opponent)
-        // this.dialogRef.close(opponent)
+        if (this.team1.length + this.team2.length >= this.data.numberParticipants)
+            return
+
+        var r = this.data.numberParticipants / 2
+        if (this.team1.length < r)
+            this.team1.push(opponent)
+        else
+            this.team2.push(opponent)
     }
 
     onSave() {
-        this.dialogRef.close(this.participants)
+        this.dialogRef.close({ team1: this.team1, team2: this.team2 })
     }
 
     onCancel(): void {
