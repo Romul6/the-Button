eda956bcdf5bb4e6f3491e9523ea29146bc33f25
Ròmul <rome.fp@gmail.com>
2024-11-17 18:54:18 +0100

    feat: vs updgrade

--- the-button/src/app/component/vs/vs.component.ts
+++ the-button/src/app/component/vs/vs.component.ts
@@ -1,19 +1,22 @@
-import { Component, EventEmitter, input } from '@angular/core';
+import { Component, EventEmitter, input, model, ModelFunction, ModelSignal } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList, CdkDragStart, } from '@angular/cdk/drag-drop';
 import { idname } from '../../models/idName';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'Btn-vs',
   standalone: true,
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag, MatIconModule, MatButtonModule],
   templateUrl: './vs.component.html',
   styleUrl: './vs.component.scss'
 })
 
 export class VsComponent {
 
-  team1 = input.required<idname[]>()
-  team2 = input.required<idname[]>()
+  team1 = model.required<idname[]>()
+  team2 = model.required<idname[]>()
+
   max4Team = input.required<number>()
 
   private currentIndexSelected = 0
@@ -25,7 +28,9 @@ export class VsComponent {
     } else {
       if (event.container.data.length >= this.max4Team()) {
         transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-        transferArrayItem(event.container.data, event.previousContainer.data, this.currentIndexSelected, 0);
+        setTimeout(() => {
+          transferArrayItem(event.container.data, event.previousContainer.data, 0, this.currentIndexSelected);
+        }, 5000);
 
       } else {
         transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
@@ -35,7 +40,10 @@ export class VsComponent {
 
   drag(started: any) {
     this.currentIndexSelected = started.currentIndex
+  }
 
+  protected onDeletePlayer(list: ModelSignal<idname[]>, event: any) {
+    list.update(v => v.filter(a => a.id != event.id))
   }
 
 }
