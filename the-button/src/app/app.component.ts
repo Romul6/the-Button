2e1cd9b66c8301ecc1599dd6b75e9414b72d7b0b
Ròmul <rome.fp@gmail.com>
2024-11-25 06:56:53 +0100

    feat

--- the-button/src/app/app.component.ts
+++ the-button/src/app/app.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HourComponentComponent } from "./component/hour/hour.component";
+import { HourComponent } from "./component/hour/hour.component";
+import { Hours } from './component/hour/hour';
+import { DayHourDividerComponent } from "./component/day-hour-divider/day-hour-divider.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HourComponentComponent],
+  imports: [CommonModule, RouterOutlet, HourComponent, DayHourDividerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'the-button';
+  // hours: Array<Hours> = [
+  //   { hour: { start: '10:00', end: '11:00' }, isBooked: true, participants: [''] },
+  //   { hour: { start: '10:00', end: '11:00' }, isBooked: true, participants: [''] },
+  //   { hour: { start: '10:00', end: '11:00' }, isBooked: false, participants: [''] }
+  // ]
+  hours: Array<Hours> = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+    var start = new Date()
+    start.setHours(0)
+    start.setMinutes(0)
+    start.setSeconds(0)
+    start.setMilliseconds(0)
+
+    var end = new Date(start)
+    end.setHours(23)
+
+    for (let index = start; index < end; index.setHours(index.getHours() + 1)) {
+      this.hours.push({
+        hour: { start: index.getHours().toString(), end: index.getHours().toString() },
+        isBooked: false,
+        participants: []
+      }
+      )
+    }
+
+  }
 }
